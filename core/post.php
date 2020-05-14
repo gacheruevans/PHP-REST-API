d968ae4a59537b995a713f8c971491a6356d36ce
egacheru <egacheru@egacherus-MacBook-Pro.local>
2020-05-14 20:01:53 +0300

    add query to db

---
+++ core/post.php
@@ -0,0 +1,43 @@
+<?php
+    class Post {
+        //db stuff
+        private $conn;
+        private $table = 'posts';
+
+
+        //post properties
+        public $id;
+        public $category_id;
+        public $category_name;
+        public $title;
+        public $body;
+        public $author;
+        public $created_at;
+
+        //constructor with db connection
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+
+        //getting posts from db
+        public function read() {
+            //fetch query
+            $query = 'SELECT c.name as category_name, 
+                p.id, 
+                p.category_id, 
+                p.title, 
+                p.body, 
+                p.author, 
+                p.created_at FROM '.$this->table. 'p 
+                LEFT JOIN 
+                    categories c ON p.category_id = c.id
+                    ORDER BY p.created_at DESC';
+
+            //prepare statement
+            $stmt = $this->conn->prepare($query);
+            //execute query
+            $stmt->execute();
+        }
+
+    }
+?>
