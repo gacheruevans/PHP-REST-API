98b62504f272446b2235d1dc5c937d4be7267759
egacheru <evans.gacheru@andela.com>
2020-05-15 16:48:29 +0300

    update function

--- core/post.php
+++ core/post.php
@@ -98,5 +98,37 @@
             return false;
         }
 
+        //Update post function
+        Public function update() {
+            /* Updtae query */
+            $query = 'UPDATE ' .$this->table.' 
+                SET title = :title, body = :body, author = :author, category_id = :category_id
+                WHERE id = :id';
+            //prepare statement
+            $stmt = $this->conn->prepare($query);
+            //clean data
+            $this->id = htmlspecialchars(strip_tags($this->id));
+            $this->title = htmlspecialchars(strip_tags($this->title));
+            $this->body = htmlspecialchars(strip_tags($this->body));
+            $this->author = htmlspecialchars(strip_tags($this->author));
+            $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+            //binding of parameters
+            $stmt->bindParam(':id', $this->id);
+            $stmt->bindParam(':title', $this->title);
+            $stmt->bindParam(':body', $this->body);
+            $stmt->bindParam(':author', $this->author);
+            $stmt->bindParam(':category_id', $this->category_id);
+
+            //execute query
+            if($stmt->execute()) {
+                return true;
+            }
+
+            //print error
+            printf('Error %s. \n', $stmt->error);
+            return false;
+        }
+
+
     }
 ?>
