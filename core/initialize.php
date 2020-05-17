71d514f1bdc96ca5dde862ec3cf6077741bb0042
egacheru <evans.gacheru@andela.com>
2020-05-17 23:21:27 +0300

    authentication path

--- core/initialize.php
+++ core/initialize.php
@@ -5,10 +5,14 @@
     //Ampps/wwww/php-rest-api/includes
     defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS.'includes');
     defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT.DS.'core');
+    defined('AUTH_PATH') ? null : define('AUTH_PATH', SITE_ROOT.DS.'core'.DS.'authentication');
 
-    //Load config files first
+    //Load DB config files first
     require_once(INC_PATH.DS.'config.php');
 
     //Core classes
     require_once(CORE_PATH.DS.'post.php');
+
+    //Authenticate classes
+    require_once(AUTH_PATH.DS.'authenticate.php');
 ?>
